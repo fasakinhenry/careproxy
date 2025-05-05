d1c8650edffa2b7a9d9848c744b64590bb3d397f
Fasakin Henry <fasakinhenry@gmail.com>
2025-05-05 02:03:15 +0100

    feat: add searchbar component

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+const SearchBar = () => {
+    return (
+      <div className="flex justify-center mt-[-2rem] mb-4">
+        <input
+          type="text"
+          placeholder="Search innovative startups..."
+          className="w-full max-w-xl p-3 rounded-md border shadow-md"
+        />
+      </div>
+    );
+  };
+  
+  export default SearchBar;
+  
